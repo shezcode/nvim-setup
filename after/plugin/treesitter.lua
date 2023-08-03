2f930f46d30903c2e370eb4df6f537060578871a
SHEZ <shezcode@gmail.com>
2023-08-03 14:39:20 +0200

    update

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "java" },
 
@@ -18,6 +18,55 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  ignore_install = { "haskell" },
+  modules = {
+    -- This enables the parser for the whole file
+    -- "maintained" parsers
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "javascript",
+    "typescript",
+    "rust",
+    "java",
+    -- "haskell",
+    -- "go",
+    -- "python",
+    -- "bash",
+    -- "json",
+    -- "html",
+    -- "css",
+    -- "comment",
+    -- "regex",
+    -- "toml",
+    -- "yaml",
+    -- "tsx",
+    -- "cpp",
+    -- "cmake",
+    -- "dockerfile",
+    -- "elixir",
+    -- "fennel",
+    -- "gdscript",
+    -- "graphql",
+    -- "hcl",
+    -- "json5",
+    -- "jsonc",
+    -- "ledger",
+    -- "ocaml",
+    -- "ocaml_interface",
+    -- "php",
+    -- "ql",
+    -- "query",
+    -- "rst",
+    -- "ruby",
+    -- "svelte",
+    -- "teal",
+    -- "turtle",
+    -- "verilog",
+    -- "vue",
+    -- "yaml",
+    -- "zig",
+  }
 }
-
-
