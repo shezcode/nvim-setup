339edfd9288d56f01350542d12246939e0598c52
SHEZ <shezcode@gmail.com>
2023-10-04 01:16:22 +0200

    new;

--- lua/shez/core/keymaps.lua
+++ lua/shez/core/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
+vim.g.copilot_no_tab_map = true
+
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") --toggle file explorer
-vim.keymap.set("n", "<leader>gp", ":ChatGPT<CR>")       --run chatGPT extension
-vim.keymap.set("n", "<leader>gr", ":ChatGPTRun")        --run chatGPT extension
-vim.keymap.set("i", "jj", "<ESC>")                      --jj to escape insert mode
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")                                 --toggle file explorer
+vim.keymap.set("n", "<leader>gp", ":ChatGPT<CR>")                                       --run chatGPT extension
+vim.keymap.set("n", "<leader>gr", ":ChatGPTRun")                                        --run chatGPT extension
+vim.keymap.set("i", "jj", "<ESC>")                                                      --jj to escape insert mode
+vim.keymap.set("i", "<C-x>", 'copilot #Accept("<CR>")', { silent = true, expr = true }) --copilot extension
--- lua/shez/core/options.lua
+++ lua/shez/core/options.lua
@@ -4,6 +4,9 @@ vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
+vim.cmd('set nocompatible')
+vim.cmd('filetype plugin on')
+vim.cmd('syntax enable')
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
